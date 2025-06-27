9c605cfa5af54d6e1a0f05e38ac716cc523197aa
Matheus Martins <contact@matheusmartins.com>
2025-06-27 08:25:19 -0300

    refactor(apps/api): Simplify behaviors and config classes

--- apps/api/Source/Config/DatabaseConfig.cs
+++ apps/api/Source/Config/DatabaseConfig.cs
@@ -1,31 +1,11 @@
 namespace GameGuild.Config;
 
 public class DatabaseConfig {
-  private string _connectionString = string.Empty;
+  public string ConnectionString { get; set; } = string.Empty;
 
-  private string _provider = "PostgreSQL";
+  public string Provider { get; set; } = "PostgreSQL";
 
-  private bool _enableSensitiveDataLogging = false;
+  public bool EnableSensitiveDataLogging { get; set; } = false;
 
-  private bool _enableDetailedErrors = false;
-
-  public string ConnectionString {
-    get => _connectionString;
-    set => _connectionString = value;
-  }
-
-  public string Provider {
-    get => _provider;
-    set => _provider = value;
-  }
-
-  public bool EnableSensitiveDataLogging {
-    get => _enableSensitiveDataLogging;
-    set => _enableSensitiveDataLogging = value;
-  }
-
-  public bool EnableDetailedErrors {
-    get => _enableDetailedErrors;
-    set => _enableDetailedErrors = value;
-  }
+  public bool EnableDetailedErrors { get; set; } = false;
 }
