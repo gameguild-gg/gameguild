9895ffd62e6a39f70c81b6f01df9408262ddb0a7
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-07-01 11:23:30 -0400

    refactor: move old code out from the current apps folder

---
+++ old/(content)/(public)/(courses)/actions.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { courses } from '@/data/courses';
+import { Api } from '@/types/course-types';
+import CourseEntity = Api.CourseEntity;
+
+export async function getCourses(): Promise<CourseEntity[]> {
+  return courses;
+}
+
+export async function getCourseBySlug(
+  slug: string,
+): Promise<CourseEntity | null> {
+  return courses.find((course) => course.slug === slug) || null;
+}
