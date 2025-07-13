8bed12fa53fb1441a67fcdfc787bddf0814f8cd2
Matheus Martins <contact@matheusmartins.com>
2025-07-13 02:33:57 -0300

    fix(tests/api): Update assembly info, category and factory usage

--- tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
+++ tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1b5bef1d5f5fdc8b722fdd81130a9669817c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d04b49f1ab23182c7b37f51254e062a143df7ce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
