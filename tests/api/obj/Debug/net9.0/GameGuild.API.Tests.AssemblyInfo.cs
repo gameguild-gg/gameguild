7512e5b89b62d61d5b57b8bdb6d34f23b5324cc7
Matheus Martins <contact@matheusmartins.com>
2025-07-15 11:07:56 -0300

    feat(apps/api): Enhance payments module and add user context

--- tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
+++ tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39078b4e014e0422b66b3edf49c9935a16b680f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e50052f0cbde0ce8d6caa24dc7523b2f5438bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
