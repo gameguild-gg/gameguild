cf9d20a96648207d8825ca281e7e7325e553a3cb
Matheus Martins <contact@matheusmartins.com>
2025-07-15 22:06:24 -0300

    fix(api/tests): Update endpoints and DbContext factory usage

--- tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
+++ tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6f115950fc084907c1cdad85808b9fc682e89f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2392646aec6bdc19058fd31f137a0944b6150333")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
