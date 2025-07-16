7bac855f11f89234adddc523de2a3ae5c693ea20
Matheus Martins <contact@matheusmartins.com>
2025-07-15 22:21:45 -0300

    chore(tests/api): Update assembly info version and cache files

--- tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
+++ tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9d20a96648207d8825ca281e7e7325e553a3cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed8fa49d357facf81cc5157a76eeaa474d3e8ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
