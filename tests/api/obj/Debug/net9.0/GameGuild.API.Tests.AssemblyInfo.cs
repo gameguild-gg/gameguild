7e6e5cde406d650c195f8ceaa392139018d5409e
Matheus Martins <contact@matheusmartins.com>
2025-07-16 20:19:29 -0300

    feat(apps/api): Add cancel payment command handler

--- tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
+++ tests/api/obj/Debug/net9.0/GameGuild.API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bfc703f9371df1e65a2258b2aebb3ec4aa4efc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86eea2d570c62669ea0d2a5318f571db47470483")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameGuild.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
