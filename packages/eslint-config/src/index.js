70173cf01fab43164a2f9b0503b717a74bff72fb
Matheus Martins <contact@matheusmartins.com>
2025-07-17 08:37:51 -0300

    refactor(config): Update shared config imports and alias paths

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,8 +1,12 @@
 import globals from 'globals';
 import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import nextJsPlugin from '@next/eslint-plugin-next';
 import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from '@game-guild/prettier-config';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import typescriptEslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -19,11 +23,45 @@ const config = [
     },
   },
   eslint.configs.recommended, // eslint recommended rules
+  eslintConfigPrettier,
   eslintPluginPrettierRecommended, // prettier recommended rules
   typescriptEslint.configs.recommended, // typescript-eslint recommended rules
   {
+    ...reactPlugin.configs.flat.recommended,
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+      },
+    },
+  },
+  {
     plugins: { prettier: prettierPlugin },
     rules: { 'prettier/prettier': ['error', prettierConfig] },
+  },
+  {
+    plugins: {
+      '@next/next': nextJsPlugin,
+    },
+    rules: {
+      ...nextJsPlugin.configs.recommended.rules,
+      ...nextJsPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...reactHooksPlugin.configs.recommended.rules,
+      // React scope no longer necessary with new JSX transform.
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+  {
+    ignores: ['dist/**'],
   },
 ];
 
