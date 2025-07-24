96b14ec1ca9dac11d9fed8cfa450a4609f03e957
Matheus Martins <contact@matheusmartins.com>
2025-07-24 19:36:10 -0300

    chore(apps/web): Reorganize routes and remove legacy testing-lab files

---
+++ apps/web/src/app/[locale]/(community)/error.tsx
@@ -0,0 +1,10 @@
+// Error boundaries must be Client Components.
+'use client';
+
+import React from 'react';
+import { Error } from '@/components/common/error';
+import { ErrorProps } from '@/types';
+
+export default function Page({ error, reset }: ErrorProps): React.JSX.Element {
+  return <Error error={error} reset={reset} />;
+}
