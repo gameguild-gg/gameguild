074cf2d4f720f79f4eb1c5f5e05b2c2caf43773c
Matheus Martins <contact@matheusmartins.com>
2025-07-24 19:49:23 -0300

    refactor(apps/web): Update ESLint config, file paths, i18n routing and middleware

--- apps/web/src/i18n/routing.ts
+++ apps/web/src/i18n/routing.ts
@@ -2,8 +2,8 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'pt-br'],
-  localePrefix: 'always',
+  locales: ['en', 'pt-BR'],
+  localePrefix: 'as-needed',
   // Used when no locale matches
   defaultLocale: 'en',
 });
