62d84acf3ac727033835188ca5dd2c259c72bbc5
Matheus Martins <contact@matheusmartins.com>
2025-07-24 19:50:43 -0300

    refactor(apps/web): Update header and footer import paths

--- apps/web/src/app/[locale]/(community)/layout.tsx
+++ apps/web/src/app/[locale]/(community)/layout.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
-import Footer from '@/components/footer';
-import Header from '@/components/header';
+import Footer from '@/components/common/footer';
+import Header from '@/components/common/header';
 
 export default async function Layout({ children }: PropsWithChildren): Promise<React.JSX.Element> {
   return (
