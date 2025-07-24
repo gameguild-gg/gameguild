a7afb77e3352d410b0d599433b58364160c193dc
Matheus Martins <contact@matheusmartins.com>
2025-07-24 01:52:13 -0300

    feat(apps/web): Add program management suite

---
+++ apps/web/src/components/programs/filters/index.ts
@@ -0,0 +1,5 @@
+export { ProgramFilterProvider, useProgramFilters } from './program-filter-context';
+export type { ProgramFilterState } from './program-filter-context';
+
+export { ProgramFilterControls } from './program-filter-controls';
+export { ProgramActiveFilters } from './program-active-filters';
