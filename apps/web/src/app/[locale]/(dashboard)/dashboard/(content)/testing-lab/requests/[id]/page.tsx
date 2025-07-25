e9a10113edfc4966f1919b5af24efb891cb196f1
Matheus Martins <contact@matheusmartins.com>
2025-07-25 08:10:43 -0300

    feat(apps/web): Wrap testing pages in dashboard layout

--- apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/testing-lab/requests/[id]/page.tsx
+++ apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/testing-lab/requests/[id]/page.tsx
@@ -3,6 +3,13 @@ import { notFound } from 'next/navigation';
 import { TestingRequestDetails } from '@/components/testing-lab';
 import { getTestingRequestById } from '@/lib/testing-lab/testing-lab.actions';
 import { PropsWithIdParams } from '@/types';
+import {
+  DashboardPage,
+  DashboardPageContent,
+  DashboardPageDescription,
+  DashboardPageHeader,
+  DashboardPageTitle,
+} from '@/components/dashboard/common/ui/dashboard-page';
 
 export default async function Page({ params }: PropsWithIdParams): Promise<React.JSX.Element> {
   const { id } = await params;
@@ -12,7 +19,15 @@ export default async function Page({ params }: PropsWithIdParams): Promise<React
 
   return (
     <>
-      <TestingRequestDetails data={testingRequest} />
+      <DashboardPage>
+        <DashboardPageHeader>
+          <DashboardPageTitle>Reports</DashboardPageTitle>
+          <DashboardPageDescription></DashboardPageDescription>
+        </DashboardPageHeader>
+        <DashboardPageContent>
+          <TestingRequestDetails data={testingRequest} />
+        </DashboardPageContent>
+      </DashboardPage>
     </>
   );
 }
