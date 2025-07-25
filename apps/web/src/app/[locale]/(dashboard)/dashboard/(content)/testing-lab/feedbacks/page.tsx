a59e25e1d3ba65828658418bf3025a5dce8f1921
Matheus Martins <contact@matheusmartins.com>
2025-07-25 00:20:40 -0300

    feat(testing-lab): Add detail components for feedback and session

--- apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/testing-lab/feedbacks/page.tsx
+++ apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/testing-lab/feedbacks/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TestingFeedbackList } from '@/components/testing-lab/testing-feedback-list';
+import { getTestingFeedbacks } from '@/lib/testing-lab/testing-lab.actions';
 
 export default async function Page(): Promise<React.JSX.Element> {
+  const testingFeedbacks = await getTestingFeedbacks();
+
   return (
     <div className="container">
-      <TestingFeedbackList />
+      <TestingFeedbackList data={testingFeedbacks} />
     </div>
   );
 }
--- apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/testing-lab/requests/page.tsx
+++ apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/testing-lab/requests/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TestingRequestList } from '@/components/testing-lab/testing-request-list';
+import { getTestingFeedbacks } from '@/lib/testing-lab/testing-lab.actions';
 
 export default async function Page(): Promise<React.JSX.Element> {
   const testingRequests = await getTestingRequests();
