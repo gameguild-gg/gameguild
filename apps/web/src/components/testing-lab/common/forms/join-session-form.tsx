c214fe05654aedce4eeed103590572e66a313ef5
Matheus Martins <contact@matheusmartins.com>
2025-07-25 04:04:59 -0300

    feat(apps/web): Add join session form component

---
+++ apps/web/src/components/testing-lab/common/forms/join-session-form.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import React from 'react';
+
+export const JoinSessionForm = (): React.JSX.Element => {
+  return <>{/* TODO: Create a form to join a session as a Developer or as a Tester. */}</>;
+};
