0f8672bc5df9876964e8e7a6c7da913419a666cb
Matheus Martins <contact@matheusmartins.com>
2025-07-25 07:06:24 -0300

    refactor(dashboard/layout): Remove dashboard provider export

--- apps/web/src/components/dashboard/layout/index.ts
+++ apps/web/src/components/dashboard/layout/index.ts
@@ -1,4 +1,4 @@
 export { Dashboard } from '../dashboard';
-export { DashboardProvider } from './dashboard-provider';
+
 export { DashboardSidebar } from '../common/ui/dashboard-sidebar';
 export { DashboardHeader } from './dashboard-header';
