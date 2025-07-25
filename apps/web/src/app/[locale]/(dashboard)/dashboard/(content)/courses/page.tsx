ced1c0a2661490205b78a01be6bae46b7a607231
Matheus Martins <contact@matheusmartins.com>
2025-07-25 14:17:19 -0300

    feat(apps/web): Add course catalog page and filtering components

--- apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/courses/page.tsx
+++ apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/courses/page.tsx
@@ -1,5 +1,25 @@
-import { redirect } from 'next/navigation';
+import React from 'react';
+import {
+  DashboardPage,
+  DashboardPageContent,
+  DashboardPageDescription,
+  DashboardPageHeader,
+  DashboardPageTitle,
+} from '@/components/dashboard/common/ui/dashboard-page';
+import { CourseList } from '@/components/courses/common/ui/course-list';
 
-export default async function Page(): Promise<void> {
-  redirect('/dashboard/courses/overview');
+export default async function Page(): Promise<React.JSX.Element> {
+  const courses = await getCourses();
+
+  return (
+    <DashboardPage>
+      <DashboardPageHeader>
+        <DashboardPageTitle>Course</DashboardPageTitle>
+        <DashboardPageDescription></DashboardPageDescription>
+      </DashboardPageHeader>
+      <DashboardPageContent>
+        <CourseList courses={courses} />
+      </DashboardPageContent>
+    </DashboardPage>
+  );
 }
