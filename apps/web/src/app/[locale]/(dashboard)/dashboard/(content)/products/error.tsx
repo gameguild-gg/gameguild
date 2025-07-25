60d6785a90e2ee870a565847a1b06b1490ce4c94
Matheus Martins <contact@matheusmartins.com>
2025-07-25 09:09:48 -0300

    refactor(dashboard): Remove unused error pages and add projects layout

--- apps/web/src/app/[locale]/(dashboard)/dashboard/(content)/products/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import { Error } from '@/components/common/error/error';
-import { ErrorProps } from '@/types';
-
-export default function Page({ error, reset }: ErrorProps): React.JSX.Element {
-  return <Error error={error} reset={reset} />;
-}
