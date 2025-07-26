df79d6cfce88c2fc8c5ef6954fe946716129f575
Matheus Martins <contact@matheusmartins.com>
2025-07-26 17:17:31 -0300

    feat(apps/web/components/content/markdown): Add markdown content component

--- apps/web/src/components/content/markdown/markdown-content.tsx
+++ apps/web/src/components/content/markdown/markdown-content.tsx
@@ -1,21 +1,36 @@
-import type React from 'react';
+import React, { type AnchorHTMLAttributes, type HTMLAttributes } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+export type MarkdownComponentType = Record<string, (props: HTMLAttributes<HTMLElement>) => React.JSX.Element>;
 
 // TODO: Migrate the tailwind classes to css classes that uses tailwind internally later.
-const components: Record<string, React.JSX.Element> = {
-  h1: <h1 className="text-4xl font-bold mt-6 mb-4" />,
-  h2: <h2 className="text-3xl font-semibold mt-5 mb-3" />,
-  h3: <h3 className="text-2xl font-semibold mt-4 mb-2" />,
-  h4: <h4 className="text-xl font-semibold mt-3 mb-2" />,
-  h5: <h5 className="text-lg font-semibold mt-2 mb-1" />,
-  h6: <h6 className="text-base font-semibold mt-2 mb-1" />,
-  p: <p className="mb-4" />,
-  ul: <ul className="list-disc pl-5 mb-4" />,
-  ol: <ol className="list-decimal pl-5 mb-4" />,
-  li: <li className="mb-1" />,
-  a: <a className="text-blue-600 hover:underline" />,
-  blockquote: <blockquote className="border-l-4 border-gray-300 pl-4 italic my-4" />,
+const baseComponents: MarkdownComponentType = {
+  h1: (props: HTMLAttributes<HTMLHeadingElement>) => <h1 className="text-foreground text-4xl font-bold mt-6 mb-4" {...props} />,
+  h2: (props: HTMLAttributes<HTMLHeadingElement>) => <h2 className="text-foreground text-3xl font-semibold mt-5 mb-3" {...props} />,
+  h3: (props: HTMLAttributes<HTMLHeadingElement>) => <h3 className="text-foreground text-2xl font-semibold mt-4 mb-2" {...props} />,
+  h4: (props: HTMLAttributes<HTMLHeadingElement>) => <h4 className="text-foreground text-xl font-semibold mt-3 mb-2" {...props} />,
+  h5: (props: HTMLAttributes<HTMLHeadingElement>) => <h5 className="text-foreground text-lg font-semibold mt-2 mb-1" {...props} />,
+  h6: (props: HTMLAttributes<HTMLHeadingElement>) => <h6 className="text-foreground text-base font-semibold mt-2 mb-1" {...props} />,
+  p: (props: HTMLAttributes<HTMLParagraphElement>) => <p className="text-foreground text-base leading-relaxed mb-4" {...props} />,
+  ul: (props: HTMLAttributes<HTMLUListElement>) => <ul className="text-foreground list-disc mb-4 pl-5" {...props} />,
+  ol: (props: HTMLAttributes<HTMLOListElement>) => <ol className="text-foreground list-decimal mb-4 pl- " {...props} />,
+  li: (props: HTMLAttributes<HTMLLIElement>) => <li className="text-foreground mb-1" {...props} />,
+  a: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => <a className="text-primary hover:underline" {...props} />,
+  blockquote: (props: HTMLAttributes<HTMLElement>) => <blockquote className="text-muted-foreground italic my-4 pl-4 border-l-4 border-border" {...props} />,
 };
 
-export const MarkdownContent = (): React.JSX.Element => {
-  return <></>;
+export interface MarkdownContentProps {
+  content: string;
+  components?: MarkdownComponentType;
+}
+
+export const MarkdownContent = ({ content, components }: MarkdownContentProps): React.JSX.Element => {
+  return (
+    <>
+      <ReactMarkdown components={{ ...baseComponents, ...components }}>
+        {/* TODO: Later add plugins and etc. so we can extend the Markdown rendering to each especific usage */}
+        {content}
+      </ReactMarkdown>
+    </>
+  );
 };
