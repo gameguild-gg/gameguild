1ec3cb45a3a5a2ed59e1108550119d1f7ce88f2b
Matheus Martins <contact@matheusmartins.com>
2025-07-26 10:28:57 -0300

    chore(tests/e2e): Remove legacy E2E tests and assets

--- apps/web/tests/test/jest.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@testing-library/jest-dom" />
-
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-
-declare module '@jest/expect' {
-  interface Matchers<R = void, T = {}> extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
-}
