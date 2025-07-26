dd7adee6536cf3a0b0fe830faa202b198c0486e3
Matheus Martins <contact@matheusmartins.com>
2025-07-26 10:23:19 -0300

    feat(apps/web): Add local auth support and update endpoints

--- apps/web/src/app/[locale]/(auth)/sign-in/page.tsx
+++ apps/web/src/app/[locale]/(auth)/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SignInForm } from '@/components/auth/common/forms/sign-in-form';
+import { SignInForm } from '@/components/auth';
 
 export default async function SignInPage(): Promise<React.JSX.Element> {
   return <SignInForm />;
