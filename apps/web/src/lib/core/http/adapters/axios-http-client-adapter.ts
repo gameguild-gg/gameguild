da89f7c1055d798d42e0b71100faf2e1730dc322
Matheus Martins <contact@matheusmartins.com>
2025-07-26 23:58:19 -0300

    chore(apps/web): Remove legacy actions and HTTP clients

--- apps/web/src/lib/core/http/adapters/axios-http-client-adapter.ts
+++
@@ -1,9 +0,0 @@
-// export class AxiosHttpClientAdapter implements HttpClient {
-//   async request<T>(data: HttpRequest): Promise<HttpResponse<T>> {
-//     return await axios.request({
-//       url: data.url,
-//       method: data.method,
-//       data: data.body,
-//     });
-//   }
-// }
