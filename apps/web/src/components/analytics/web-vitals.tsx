8e505c8f973c4c1495d1c6f8b7abb78a5257377a
Matheus Martins <contact@matheusmartins.com>
2025-07-26 23:49:02 -0300

    feat(analytics/web-vitals): Enable sendBeacon and fetch reporting

--- apps/web/src/components/analytics/web-vitals.tsx
+++ apps/web/src/components/analytics/web-vitals.tsx
@@ -3,23 +3,24 @@
 import { useReportWebVitals } from 'next/web-vitals';
 import { sendGAEvent } from '@next/third-parties/google';
 
+const WEB_VITALS_ENDPOINT = '/api/analytics/web-vitals'; // Replace with your actual endpoint
+
 export const WebVitals = (): null => {
   useReportWebVitals((metric) => {
     const payload = JSON.stringify(metric);
 
+    // TODO: Uncomment this when the endpoint is ready.
     // Try to send the metric to an analytics endpoint
     // using `navigator.sendBeacon` if available.
-    // TODO: Uncomment this when the endpoint is ready.
-    // if (navigator.sendBeacon) {
-    //   navigator.sendBeacon('/api/web‑vitals', payload);
-    // } else {
-    //   void fetch('/api/web‑vitals', {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: payload,
-    //     keepalive: true,
-    //   });
-    // }
+    if (navigator.sendBeacon) navigator.sendBeacon(WEB_VITALS_ENDPOINT, payload);
+    else {
+      void fetch(WEB_VITALS_ENDPOINT, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: payload,
+        keepalive: true,
+      });
+    }
 
     sendGAEvent('event', metric.name, {
       value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value),
