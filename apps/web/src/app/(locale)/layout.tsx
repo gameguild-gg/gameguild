1309e3dd564aa5157dca40eff6c6646543145fde
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-07-31 16:52:07 -0400

    fix(web): add newrelic

--- apps/web/src/app/(locale)/layout.tsx
+++ apps/web/src/app/(locale)/layout.tsx
@@ -1,14 +1,15 @@
-import React, { PropsWithChildren } from 'react';
+import { auth } from '@/auth';
+import { WebVitals } from '@/components/analytics';
+import { NewRelicProvider } from '@/components/analytics/newrelic-provider';
+import { ErrorBoundaryProvider } from '@/components/common/error/error-boundary-provider';
+import { TenantProvider } from '@/components/tenant';
+import { ThemeProvider } from '@/components/theme';
+import { Toaster } from '@/components/ui/sonner';
+import { Web3Provider } from '@/components/web3/web3-context';
+import { environment } from '@/configs/environment';
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import { SessionProvider } from 'next-auth/react';
-import { WebVitals } from '@/components/analytics';
-import { ThemeProvider } from '@/components/theme';
-import { Web3Provider } from '@/components/web3/web3-context';
-import { TenantProvider } from '@/components/tenant';
-import { ErrorBoundaryProvider } from '@/components/common/error/error-boundary-provider';
-import { environment } from '@/configs/environment';
-import { auth } from '@/auth';
-import { Toaster } from '@/components/ui/sonner';
+import React, { PropsWithChildren } from 'react';
 
 // TODO: Uncomment this when you have the metadata fetching logic ready.
 // export async function generateMetadata(): Promise<Metadata> {
@@ -38,12 +39,14 @@ export default async function Layout({ children }: PropsWithChildren): Promise<R
             <Web3Provider>
               <SessionProvider session={session}>
                 <TenantProvider initialState={{ currentTenant: session?.currentTenant, availableTenants: session?.availableTenants }}>
-                  {/*TODO: Move this to a better place*/}
-                  {/*<ThemeToggle />*/}
-                  {children}
-                  {/*TODO: Move this to a better place*/}
-                  {/*<FeedbackFloatingButton />*/}
-                  <Toaster />
+                  <NewRelicProvider>
+                    {/*TODO: Move this to a better place*/}
+                    {/*<ThemeToggle />*/}
+                    {children}
+                    {/*TODO: Move this to a better place*/}
+                    {/*<FeedbackFloatingButton />*/}
+                    <Toaster />
+                  </NewRelicProvider>
                 </TenantProvider>
               </SessionProvider>
             </Web3Provider>
