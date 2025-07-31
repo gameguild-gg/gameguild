4d11c7e525d1656b135fd2c5e41d34b5cfbe0e5a
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-07-31 13:00:41 -0400

    fix(web): adjust docker build on apps/web

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,13 +1,6 @@
-import React from 'react';
 import { NotFound } from '@/components/common/error/not-found';
+import React from 'react';
 
 export default async function Page(): Promise<React.JSX.Element> {
-  return (
-    // root not-found must include HTML and body tags.
-    <html lang="en">
-      <body>
-        <NotFound />
-      </body>
-    </html>
-  );
+  return <NotFound />;
 }
