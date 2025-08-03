8377d3032128593467d68946c82cbab4504d62db
Matheus Martins <contact@matheusmartins.com>
2025-08-03 18:18:55 -0300

    oid

--- old/services/api/src/program/modules/tag/tag.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class TagService {
-  // TODO: Implement service methods
-}
--- old/services/api/src/program/services/dtos.ts
+++
@@ -1,4 +0,0 @@
-export { CreateProgramDto } from './create-program.dto';
-export { UpdateProgramDto } from './update-program.dto';
-export { ProgramFilters } from './program-filters.dto';
-export { ProgramStats } from './program-stats.dto';
