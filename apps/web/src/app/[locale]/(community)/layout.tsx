a42109b8539d97e3cffeb2d4775d53ec9a629ce0
Matheus Martins <contact@matheusmartins.com>
2025-08-23 19:02:19 -0300

    refactor(web): Restructure application routes

--- apps/web/src/app/[locale]/(community)/layout.tsx
+++ apps/web/src/app/[locale]/(community)/layout.tsx
@@ -1,9 +1,10 @@
 import Footer from '@/components/common/footer/default-footer';
 import { Header } from '@/components/common/header';
+import React, { PropsWithChildren } from 'react';
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: PropsWithChildren): Promise<React.JSX.Element> {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
+    <div className="flex flex-col flex-1 bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
       <div className="flex-1 flex flex-col">
         <Header />
         <div className="flex flex-col flex-1">
