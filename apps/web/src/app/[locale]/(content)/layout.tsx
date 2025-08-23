40dbd3d9fb1fc946e1bee1874b76b9bb13f0d1f1
Matheus Martins <contact@matheusmartins.com>
2025-08-23 20:55:16 -0300

    feat(apps/web): Add testing session sidebar and improve layout structure

--- apps/web/src/app/[locale]/(content)/layout.tsx
+++ apps/web/src/app/[locale]/(content)/layout.tsx
@@ -1,15 +1,17 @@
-import Footer from '@/components/common/footer/default-footer';
-import {Header} from '@/components/common/header';
 import React, {PropsWithChildren} from "react";
+import {Footer} from '@/components/common/footer/default-footer';
+import {Header} from '@/components/common/header';
 
 export default async function Layout({children}: PropsWithChildren): Promise<React.JSX.Element> {
   return (
-    <div className="flex flex-col flex-1 bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
-      <Header/>
-      <div className="flex flex-col flex-1">
-        {children}
+    <>
+      <div className="flex flex-col flex-1 bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
+        <Header/>
+        <div className="flex flex-col flex-1">
+          {children}
+        </div>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>
+    </>
   );
 }
