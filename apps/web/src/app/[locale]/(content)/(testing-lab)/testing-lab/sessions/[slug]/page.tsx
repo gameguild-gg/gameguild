a11cdee3092e7400d412e8a8acc365b3f5631bc1
Matheus Martins <contact@matheusmartins.com>
2025-08-23 20:55:16 -0300

    feat(apps/web): Add testing session sidebar and improve layout structure

--- apps/web/src/app/[locale]/(content)/(testing-lab)/testing-lab/sessions/[slug]/page.tsx
+++ apps/web/src/app/[locale]/(content)/(testing-lab)/testing-lab/sessions/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import {notFound} from 'next/navigation';
+import {notFound} from "next/navigation";
+
 import {SessionDetail} from "@/components/testing-lab/sessions/session-detail";
+import {
+  TestingSessionDetailSidebar
+} from "@/app/[locale]/(content)/(testing-lab)/components/testing-session-details-sidebar";
+import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar";
 import {getTestSessionBySlug} from "@/lib";
 import {PropsWithSlugParams} from "@/types";
 
@@ -14,6 +19,21 @@ export default async function Page({params}: PropsWithSlugParams): Promise<React
   return (
     <>
       <SessionDetail session={session}/>
+      <SidebarProvider>
+        {/* Collapsible Sidebar */}
+        <TestingSessionDetailSidebar session={session}/>
+        <SidebarInset>
+          {/* Main Content Area */}
+          <main>
+
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+      {/* Testing Requirements Sheet */}
+
+      {/* Game Details Sheet */}
+
+      {/* Join Session Modal */}
     </>
   );
 }
