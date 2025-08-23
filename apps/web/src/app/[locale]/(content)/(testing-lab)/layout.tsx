794d7c22e2c2ab1113d90c0c803a8b90f233a7fa
Matheus Martins <contact@matheusmartins.com>
2025-08-23 19:19:49 -0300

    refactor(apps/web): Move testing-lab files under (content) directory and simplify layout component

---
+++ apps/web/src/app/[locale]/(content)/(testing-lab)/layout.tsx
@@ -0,0 +1,9 @@
+import React, {PropsWithChildren} from "react";
+
+export default async function Layout({children}: PropsWithChildren): Promise<React.JSX.Element> {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
