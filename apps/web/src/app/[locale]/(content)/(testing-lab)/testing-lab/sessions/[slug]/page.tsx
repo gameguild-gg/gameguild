be3f204ba55af7ad9ce51919e4e76212e42317f2
Matheus Martins <contact@matheusmartins.com>
2025-08-23 20:10:35 -0300

    refactor(apps/web/src/app/[locale]/(content)/(testing-lab)/testing-lab/sessions/[slug]): Simplify session page component typings

--- apps/web/src/app/[locale]/(content)/(testing-lab)/testing-lab/sessions/[slug]/page.tsx
+++ apps/web/src/app/[locale]/(content)/(testing-lab)/testing-lab/sessions/[slug]/page.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import {notFound} from 'next/navigation';
 import {SessionDetail} from "@/components/testing-lab/sessions/session-detail";
 import {getTestSessionBySlug} from "@/lib";
+import {PropsWithSlugParams} from "@/types";
 
-interface Props {
-  params: Promise<{
-    slug: string;
-  }>;
-}
+export default async function Page({params}: PropsWithSlugParams): Promise<React.JSX.Element> {
+  const {slug} = await params;
 
-export default async function Page({params}: Props): Promise<React.JSX.Element> {
-  const { slug } = await params;
+  const session = await getTestSessionBySlug(slug);
 
-    const session = await getTestSessionBySlug(slug);
+  if (!session) notFound();
 
-    if (!session) notFound();
-
-    return <SessionDetail session={session}/>;
+  return (
+    <>
+      <SessionDetail session={session}/>
+    </>
+  );
 }
