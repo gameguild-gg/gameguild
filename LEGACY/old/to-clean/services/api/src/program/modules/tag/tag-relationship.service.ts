f3c54a648a330df9fc15e460aab89882fd2d5c29
Matheus Martins <contact@matheusmartins.com>
2025-08-26 08:53:54 -0300

    chore(legacy): Remove deprecated legacy API and web service code and assets

--- LEGACY/old/to-clean/services/api/src/program/modules/tag/tag-relationship.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class TagRelationshipService {
-  // TODO: Implement service methods
-}
--- LEGACY/old/to-clean/services/api/src/program/services/update-program.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProgramDto } from './create-program.dto';
-
-export class UpdateProgramDto extends PartialType(CreateProgramDto) {
-  // All fields from CreateProgramDto are automatically made optional by PartialType
-}
