90c37101c27ba15c95875aaf3481dbb50f2dc254
Matheus Martins <contact@matheusmartins.com>
2025-08-28 23:39:05 -0300

    refactor(apps/web): Remove user management and permissions components and related code

--- apps/web/src/components/users/filters/index.ts
+++
@@ -1,5 +0,0 @@
-export { UserFilterProvider, useUserFilters } from './user-filter-context';
-export type { UserFilterState } from './user-filter-context';
-
-export { UserFilterControls } from './user-filter-controls';
-export { UserActiveFilters } from './user-active-filters';
