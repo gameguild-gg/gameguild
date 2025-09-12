108c9ab3180ac4735dbe140a7cbdcf5a39050ee7
Matheus Martins <contact@matheusmartins.com>
2025-09-12 18:43:54 -0300

    Introduce domain events infrastructure with publishers and dispatcher; add aggregate metadata

--- apps/api/Source/CQRS/Abstractions/Events/IDomainEventHandler.cs
+++ apps/api/Source/CQRS/Abstractions/Events/IDomainEventHandler.cs
@@ -4,5 +4,4 @@ namespace GameGuild.CQRS;
 /// Defines a handler for domain events
 /// </summary>
 /// <typeparam name="TDomainEvent">The type of domain event being handled</typeparam>
-public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent>
-    where TDomainEvent : IDomainEvent { }
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent> where TDomainEvent : IDomainEvent { }
---
+++ apps/api/Source/CQRS/Abstractions/Events/IDomainEventsDispatcher.cs
@@ -0,0 +1,5 @@
+﻿namespace GameGuild.CQRS;
+
+public interface IDomainEventsDispatcher {
+  Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+}
