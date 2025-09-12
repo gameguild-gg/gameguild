b00d4c3b099c80a25d943c8853676dd196a335c8
Matheus Martins <contact@matheusmartins.com>
2025-09-12 13:49:59 -0300

    Add domain exception hierarchy and business rule exceptions

---
+++ apps/api/Source/Core/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+namespace GameGuild;
+
+/// <summary>
+///     Base domain exception
+/// </summary>
+public abstract class DomainException : Exception {
+  protected DomainException(string message) : base(message) { }
+
+  protected DomainException(string message, Exception innerException) : base(message, innerException) { }
+}
