cef7efaac4577bf8806da73ef6f62cfa75679d32
Matheus Martins <contact@matheusmartins.com>
2025-09-12 18:46:47 -0300

    feat(subscriptions): add subscription types/intervals/statuses, acquisition/access, and promos

---
+++ apps/api/Source/Enums/PromoCodeType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+
+namespace GameGuild.Common;
+
+public enum PromoCodeType {
+  [Description("Applies a percentage discount to the total price")]
+  PercentageOff,
+
+  [Description("Deducts a specific amount from the total price")]
+  FixedAmountOff,
+
+  [Description("Purchase one product and receive another free")]
+  BuyOneGetOne,
+
+  [Description("No charge for the first month of a subscription")]
+  FirstMonthFree,
+}
