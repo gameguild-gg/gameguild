a53cc6c2964eb5b12bbecec57e44e5fb3a335239
Matheus Martins <contact@matheusmartins.com>
2025-09-12 18:46:45 -0300

    feat(learning): add program and product learning domain enums

---
+++ apps/api/Source/Enums/EnrollmentStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+
+namespace GameGuild.Common;
+
+public enum EnrollmentStatus {
+  [Description("Open for enrollment")] Open,
+  [Description("Closed for enrollment")] Closed,
+  [Description("Invite only")] InviteOnly,
+  [Description("Waitlist available")] Waitlist,
+}
