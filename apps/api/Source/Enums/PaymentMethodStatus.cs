219e7c6cceaa15d9936a93065bf1f1db8d9576b2
Matheus Martins <contact@matheusmartins.com>
2025-09-12 18:46:46 -0300

    feat(payments): add gateways, methods, transaction types/statuses, and wallet status

---
+++ apps/api/Source/Enums/PaymentMethodStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+
+namespace GameGuild.Common;
+
+public enum PaymentMethodStatus {
+  [Description("Payment method is valid and can be used")]
+  Active,
+
+  [Description("Payment method temporarily disabled")]
+  Inactive,
+
+  [Description("Payment method has expired (e.g., expired card)")]
+  Expired,
+
+  [Description("Payment method has been deleted by the user")]
+  Removed,
+}
