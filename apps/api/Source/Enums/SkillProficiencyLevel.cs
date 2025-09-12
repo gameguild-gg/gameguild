386ec0109ba596a8717ddd4542a2426479c658db
Matheus Martins <contact@matheusmartins.com>
2025-09-12 18:46:44 -0300

    feat(tags): add TagType, TagRelationshipType, and SkillProficiencyLevel enums

---
+++ apps/api/Source/Enums/SkillProficiencyLevel.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel;
+
+
+namespace GameGuild.Common;
+
+public enum SkillProficiencyLevel {
+  [Description("Basic awareness and understanding of concepts")]
+  Awareness,
+
+  [Description("Can perform basic tasks with guidance")]
+  Novice,
+
+  [Description("Can perform routine tasks independently")]
+  Beginner,
+
+  [Description("Can handle most tasks and some complex scenarios")]
+  Intermediate,
+
+  [Description("Can handle complex tasks and mentor others")]
+  Advanced,
+
+  [Description("Deep expertise, can innovate and teach")]
+  Expert,
+
+  [Description("Recognized authority, can define best practices")]
+  Master,
+}
