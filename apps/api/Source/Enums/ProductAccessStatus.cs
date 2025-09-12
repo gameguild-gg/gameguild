d1d391eff63c84da676c9fd11819ea489d91c45e
Matheus Martins <contact@matheusmartins.com>
2025-09-12 18:46:47 -0300

    feat(subscriptions): add subscription types/intervals/statuses, acquisition/access, and promos

---
+++ apps/api/Source/Enums/ProductAccessStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+
+namespace GameGuild.Common;
+
+public enum ProductAccessStatus {
+  [Description("User has full access to the product")]
+  Active,
+
+  [Description("Access period has ended")]
+  Expired,
+
+  [Description("Access manually removed by admin")]
+  Revoked,
+
+  [Description("Temporary hold on access that may be restored")]
+  Suspended,
+}
