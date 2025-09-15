1c57541fe6e2c2b72a58bb06b74edba9ab80dd29
Matheus Martins <contact@matheusmartins.com>
2025-09-15 16:31:37 -0300

    Unify Authentication CQRS namespace usage and formatting

--- apps/api/Source/Modules/Authentication/CQRS/Commands/VerifyWeb3SignatureCommand.cs
+++ apps/api/Source/Modules/Authentication/CQRS/Commands/VerifyWeb3SignatureCommand.cs
@@ -1,9 +1,12 @@
+using GameGuild.CQRS;
+
 namespace GameGuild.Modules.Authentication;
 
 /// <summary>
 /// Command to verify Web3 signature and authenticate a user
 /// </summary>
-public class VerifyWeb3SignatureCommand : IRequest<SignInResponseDto> {
+public class VerifyWeb3SignatureCommand : IRequest<SignInResponseDto>
+{
   public string WalletAddress { get; set; } = string.Empty;
 
   public string Signature { get; set; } = string.Empty;
