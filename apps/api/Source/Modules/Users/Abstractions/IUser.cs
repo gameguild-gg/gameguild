b45928cda1ff06d54748df65daf1d9a22d7fd7b5
Matheus Martins <contact@matheusmartins.com>
2025-09-15 17:40:19 -0300

    Introduce IUser abstraction and align TokenProvider and User entity

---
+++ apps/api/Source/Modules/Users/Abstractions/IUser.cs
@@ -0,0 +1,12 @@
+namespace GameGuild.Modules.Users;
+
+/// <summary>
+/// Interface representing a user in the system
+/// </summary>
+public interface IUser
+{
+    Guid Id { get; }
+    string Name { get; }
+    string Username { get; }
+    string Email { get; }
+}
