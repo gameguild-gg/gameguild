49d37f65822b6284deafa5a9336388d2cd310e56
Matheus Martins <contact@matheusmartins.com>
2025-09-15 13:26:47 -0300

    Resources: add EF Core and DataAnnotations usings to models

--- apps/api/Source/Modules/Resources/Models/ResourceUsageRecord.cs
+++ apps/api/Source/Modules/Resources/Models/ResourceUsageRecord.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace GameGuild.Modules.Resources.Models;
 
 /// <summary>
