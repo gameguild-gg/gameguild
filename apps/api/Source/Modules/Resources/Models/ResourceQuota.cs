10bd9e9ed38111f583345595587e128b243ac238
Matheus Martins <contact@matheusmartins.com>
2025-09-17 20:05:33 -0300

    chore: remove unused usings across modules

--- apps/api/Source/Modules/Resources/Models/ResourceQuota.cs
+++ apps/api/Source/Modules/Resources/Models/ResourceQuota.cs
@@ -1,8 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-
-
 namespace GameGuild.Modules.Resources.Models;
 
 /// <summary>
