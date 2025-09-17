027fc276871674633e54d6d6c72218e4bec74081
Matheus Martins <contact@matheusmartins.com>
2025-09-17 10:53:19 -0300

    refactor(apps/api): Remove deprecated enums and unused configuration classes

--- apps/api/Source/Common/Configuration/RegistrationMetrics.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameGuild;
-
-public class RegistrationMetrics {
-  public int TotalHandlersRegistered { get; set; }
-
-  public int TotalValidatorsRegistered { get; set; }
-
-  public TimeSpan RegistrationDuration { get; set; }
-}
