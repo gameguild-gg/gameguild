728f3184849d6d7195f59d430766aba8fb34eeee
Matheus Martins <contact@matheusmartins.com>
2025-09-17 20:05:32 -0300

    refactor(core): simplify CustomResults type references

--- apps/api/Source/Core/Results/CustomResults.cs
+++ apps/api/Source/Core/Results/CustomResults.cs
@@ -1,4 +1,3 @@
-using GameGuild.CQRS;
 using CqrsResult = GameGuild.CQRS.Result;
 
 
@@ -7,13 +6,14 @@ namespace GameGuild;
 public static class CustomResults {
   public static Microsoft.AspNetCore.Http.IResult Problem(CqrsResult result) {
     if (result.IsSuccess) throw new InvalidOperationException();
+
     if (result.Error == null) throw new InvalidOperationException("Error cannot be null for failed result");
 
     return Results.Problem(title : GetTitle(result.Error), detail : GetDetail(result.Error), type : GetType(), statusCode : GetStatusCode());
 
-    static string GetTitle(GameGuild.CQRS.Error error) { return error.Code; }
+    static string GetTitle(Error error) { return error.Code; }
 
-    static string GetDetail(GameGuild.CQRS.Error error) { return error.Message; }
+    static string GetDetail(Error error) { return error.Message; }
 
     static string GetType() { return "https://tools.ietf.org/html/rfc7231#section-6.5.1"; }
 
