ac4cb0a83b6b1413110b3f945137e97e1682fa56
Matheus Martins <contact@matheusmartins.com>
2025-09-18 08:15:09 -0300

    Projects: add missing DataAnnotations/Schema/EF usings and EF Core import in service

--- apps/api/Source/Modules/Projects/Models/ProjectCategory.cs
+++ apps/api/Source/Modules/Projects/Models/ProjectCategory.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using GameGuild.Modules.Resources;
 
 
--- apps/api/Source/Modules/Projects/Models/ProjectRelease.cs
+++ apps/api/Source/Modules/Projects/Models/ProjectRelease.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using GameGuild.Modules.Contents;
 using GameGuild.Modules.Resources;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace GameGuild.Modules.Projects;
