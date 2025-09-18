83d8dd99e92749fb8a37c8151ccd70eb05dc64c3
Matheus Martins <contact@matheusmartins.com>
2025-09-18 04:16:07 -0300

    chore: remove unused and duplicate usings across core and modules

--- apps/api/Source/Modules/Authorization/RequireDacPermissionAttribute.cs
+++ apps/api/Source/Modules/Authorization/RequireDacPermissionAttribute.cs
@@ -1,6 +1,5 @@
 using System.Security.Claims;
 using GameGuild.Modules.Authentication;
-using GameGuild.Modules.Permissions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
