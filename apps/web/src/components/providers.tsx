153ba60035bdafb63c970f405b60df65cf8ed996
Matheus Martins <contact@matheusmartins.com>
2025-09-20 13:54:54 -0300

    web(tenants): add React Query layer and migrate tenant pages

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,16 +1,19 @@
 "use client"
 
-import React, {PropsWithChildren} from 'react'
-import {QueryClientProvider} from '@tanstack/react-query'
-import {getQueryClient} from "@/components/get-query-client";
+import { getQueryClient } from "@/components/get-query-client";
+import { ReduxProvider } from "@/components/providers/redux-provider";
+import { QueryClientProvider } from '@tanstack/react-query';
+import React, { PropsWithChildren } from 'react';
 
-export function Providers({children}: PropsWithChildren): React.JSX.Element {
-    const queryClient = getQueryClient();
+export function Providers({ children }: PropsWithChildren): React.JSX.Element {
+  const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <ReduxProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </ReduxProvider>
   )
 }
 
