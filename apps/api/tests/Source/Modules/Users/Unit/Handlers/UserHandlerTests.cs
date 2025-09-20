b0ddf8d62c8136f8f7bc809677221c7ba7356960
Matheus Martins <contact@matheusmartins.com>
2025-09-20 13:54:52 -0300

    tests(users): reorder using directives in user tests

--- apps/api/tests/Source/Modules/Users/Unit/Handlers/UserHandlerTests.cs
+++ apps/api/tests/Source/Modules/Users/Unit/Handlers/UserHandlerTests.cs
@@ -1,6 +1,6 @@
+using GameGuild.CQRS;
 using GameGuild.Database;
 using GameGuild.Modules.Users;
-using GameGuild.CQRS;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
