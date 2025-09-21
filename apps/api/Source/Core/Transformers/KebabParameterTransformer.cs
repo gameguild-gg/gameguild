b4bbb24527731daa9c5aee10b920badfbd209a9e
Matheus Martins <contact@matheusmartins.com>
2025-09-21 16:37:22 -0300

    Routing: improve kebab-case transformation and tests

--- apps/api/Source/Core/Transformers/KebabParameterTransformer.cs
+++ apps/api/Source/Core/Transformers/KebabParameterTransformer.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Routing;
+
 namespace GameGuild;
 
 /// <summary> Transforms route parameters from PascalCase to kebab-case Example: "TenantRoles" becomes "tenant-roles" </summary>
 public class KebabParameterTransformer : IOutboundParameterTransformer {
-  private static readonly KebabCaseTransformer Transformer = new KebabCaseTransformer();
+  private static readonly KebabCaseTransformer Transformer = new();
 
   public string? TransformOutbound(object? value) { return value is not string s ? null : Transformer.Transform(s); }
 }
