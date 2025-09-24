90e2a82adf890f529c88c8e5f63bb1470d2299d3
Matheus Martins <contact@matheusmartins.com>
2025-09-24 03:03:45 -0300

    feat(web): improve Apollo provider auth flow and provider composition

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -1,24 +1,24 @@
-import {auth} from '@/auth';
-import {WebVitals} from '@/components/analytics';
-import {ErrorBoundaryProvider} from '@/components/common/errors/error-boundary-provider';
-import {Providers} from '@/components/providers';
-import {ApolloClientProvider} from '@/components/providers/apollo-provider';
-import {GitHubIssueProvider} from '@/components/providers/github-issue-provider';
-import {TenantProvider} from '@/components/tenant';
-import {ThemeProvider} from '@/components/theme';
-import {Toaster} from '@/components/ui/sonner';
-import {Web3Provider} from '@/components/web3';
-import {environment} from '@/configs/environment';
-import {routing} from '@/i18n';
-import {PropsWithLocaleParams} from '@/types';
-import {GoogleAnalytics, GoogleTagManager} from '@next/third-parties/google';
-import {Metadata} from 'next';
-import {SessionProvider} from 'next-auth/react';
-import {hasLocale, NextIntlClientProvider} from 'next-intl';
-import {getMessages, setRequestLocale} from 'next-intl/server';
-import {Inter} from "next/font/google";
-import {notFound} from 'next/navigation';
-import React, {PropsWithChildren} from 'react';
+import { auth } from '@/auth';
+import { WebVitals } from '@/components/analytics';
+import { ErrorBoundaryProvider } from '@/components/common/errors/error-boundary-provider';
+import { Providers } from '@/components/providers';
+import { ApolloClientProvider } from '@/components/providers/apollo-provider';
+import { GitHubIssueProvider } from '@/components/providers/github-issue-provider';
+import { TenantProvider } from '@/components/tenant';
+import { ThemeProvider } from '@/components/theme';
+import { Toaster } from '@/components/ui/sonner';
+import { Web3Provider } from '@/components/web3';
+import { environment } from '@/configs/environment';
+import { routing } from '@/i18n';
+import { PropsWithLocaleParams } from '@/types';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import { Metadata } from 'next';
+import { SessionProvider } from 'next-auth/react';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { getMessages, setRequestLocale } from 'next-intl/server';
+import { Inter } from "next/font/google";
+import { notFound } from 'next/navigation';
+import React, { PropsWithChildren } from 'react';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -67,9 +67,9 @@ export default async function Layout({ children, params }: PropsWithChildren<Pro
             <ErrorBoundaryProvider config={{ level: 'page', enableRetry: true, maxRetries: 3, reportToAnalytics: true, isolate: false }}>
               {/* TODO: If the session has a user and it has signed-in by a web3 address then try to connect to the wallet address. */}
               <Providers>
-                <ApolloClientProvider>
-                  <Web3Provider>
-                    <SessionProvider session={session}>
+                <Web3Provider>
+                  <SessionProvider session={session}>
+                    <ApolloClientProvider>
                       <TenantProvider initialState={{ currentTenant: session?.currentTenant, availableTenants: session?.availableTenants }}>
                         {/*TODO: Move this to a better place*/}
                         {/*<ThemeToggle />*/}
@@ -79,9 +79,9 @@ export default async function Layout({ children, params }: PropsWithChildren<Pro
                         {/*<FeedbackFloatingButton />*/}
                         <Toaster />
                       </TenantProvider>
-                    </SessionProvider>
-                  </Web3Provider>
-                </ApolloClientProvider>
+                    </ApolloClientProvider>
+                  </SessionProvider>
+                </Web3Provider>
               </Providers>
             </ErrorBoundaryProvider>
           </ThemeProvider>
