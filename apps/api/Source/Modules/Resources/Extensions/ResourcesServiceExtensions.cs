08665cf94a4c7e8d61e265b62cbe7535e92b88ef
Matheus Martins <contact@matheusmartins.com>
2025-10-10 14:34:13 -0300

    I

--- apps/api/Source/Modules/Resources/Extensions/ResourcesServiceExtensions.cs
+++ apps/api/Source/Modules/Resources/Extensions/ResourcesServiceExtensions.cs
@@ -1,6 +1,6 @@
 using GameGuild.CQRS;
 using GameGuild.Modules.Resources.Commands;
-using GameGuild.Modules.Resources.Entities;
+using GameGuild.Modules.Resources;
 using GameGuild.Modules.Resources.Handlers;
 using GameGuild.Modules.Resources.Infrastructure;
 using GameGuild.Modules.Resources.Repositories;
