236362d90b834f538bf8415c02e79d826e97cc9a
Matheus Martins <contact@matheusmartins.com>
2025-10-10 22:35:38 -0300

    asdasdas

--- apps/api/Source/Modules/Payments/Payments.Application/Commands/LockWalletCommand.cs
+++ apps/api/Source/Modules/Payments/Payments.Application/Commands/LockWalletCommand.cs
@@ -5,7 +5,7 @@ namespace GameGuild.Modules.Payments.Commands;
 /// <summary>
 ///     Lock a user's wallet
 /// </summary>
-public record LockWalletCommand : IRequest
+public record LockWalletCommand : ICommand<Unit>
 {
     public required Guid UserId { get; init; }
     public required string Reason { get; init; }
