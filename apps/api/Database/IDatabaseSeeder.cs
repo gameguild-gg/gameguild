799e37c33b3de482b4a192a6fd6ed821f4dabcc9
Matheus Martins <contact@matheusmartins.com>
2025-11-13 09:58:01 -0300

    OLD MIGRATIONS REMOVAL

--- apps/api/Database/IDatabaseSeeder.cs
+++
@@ -1,8 +0,0 @@
-namespace GameGuild.Core.Domain.Services;
-
-/// <summary> Interface for database seeding operations </summary>
-public interface IDatabaseSeeder {
-  /// <summary> Seeds the database with initial data </summary>
-  /// <returns> A task representing the async operation </returns>
-  Task SeedAsync();
-}
