ba5de8d1b79035ef4794542041065fbb5839d06f
Matheus Martins <contact@matheusmartins.com>
2025-11-13 09:51:58 -0300

    TEMP REMOVAL

--- temp/LEGACY/old/quick-fix/old/editor/editor.tsx
+++
@@ -1,6 +0,0 @@
-// import ToolbarPlugin from '@/lib/old/editor/plugins/ToolbarPlugin';
-// import TreeViewPlugin from '@/lib/old/editor/plugins/TreeViewPlugin';
-// import CodeHighlightPlugin from '@/lib/old/editor/plugins/CodeHighlightPlugin';
-// import ListMaxIndentLevelPlugin from '@/lib/old/editor/plugins/ListMaxIndentLevelPlugin';
-
-// todo: export editor
