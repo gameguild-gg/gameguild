b76c53dae612258ed2b69541735a4403858f6563
Matheus Martins <contact@matheusmartins.com>
2025-11-25 10:21:53 -0300

    Adds program content interaction services
    Defines interfaces and commands for program management, content interaction tracking, activity grading, peer review, and content reporting.
    
    These interfaces abstract core functionalities for interacting with programs and content within them.
    The services are designed to support various educational activities.

--- apps/api/Source/Modules/GameGuild.Programs/GlobalUsings.cs
+++ apps/api/Source/Modules/GameGuild.Programs/GlobalUsings.cs
@@ -1,4 +1,19 @@
 // Global usings for Programs module
-// Explicitly import Entities namespace to make Program resolve to Entities.Program
-// The implicit Program class from top-level statements is internal and in global namespace
+global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using FluentValidation;
+global using GameGuild.Authentication.Enums;
+global using GameGuild.CQRS;
 global using GameGuild.Modules.Programs.Entities;
+global using GameGuild.SharedKernel;
+global using GameGuild.SharedKernel.Enums;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using ApplicationDbContext = GameGuild.API.Data.ApplicationDbContext;
