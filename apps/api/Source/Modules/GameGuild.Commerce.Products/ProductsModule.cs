7ea452945238351f730ab67d04d2b253ce51de4c
Matheus Martins <contact@matheusmartins.com>
2026-01-13 20:57:53 -0300

    feat: Move order-related services to GameGuild.Commerce.Orders module

--- apps/api/Source/Modules/GameGuild.Commerce.Products/ProductsModule.cs
+++ apps/api/Source/Modules/GameGuild.Commerce.Products/ProductsModule.cs
@@ -18,16 +18,15 @@ public static class ProductsModule
         services.AddScoped<IProductPricingRepository, ProductPricingRepository>();
         services.AddScoped<IPromoCodeRepository, PromoCodeRepository>();
         services.AddScoped<IUserProductRepository, UserProductRepository>();
-        services.AddScoped<IOrderRepository, OrderRepository>();
 
         // Register services
         services.AddScoped<IPricingEngineService, PricingEngineService>();
         services.AddScoped<IProductPricingService, ProductPricingService>();
         services.AddScoped<IPromoCodeService, PromoCodeService>();
         services.AddScoped<IUserProductService, UserProductService>();
-        services.AddScoped<IOrderService, OrderService>();
         services.AddScoped<IEntitlementService, EntitlementService>();
 
+        // Note: Order-related services are now in GameGuild.Commerce.Orders module
         // CQRS handlers are automatically registered by assembly scanning in ApplicationLayerExtensions
 
         return services;
@@ -145,58 +144,6 @@ public static class ProductsModule
             entity.Property(e => e.MaxTotalDiscountAmount).HasPrecision(10, 2);
         });
 
-        modelBuilder.Entity<Order>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.HasIndex(e => e.UserId);
-            entity.HasIndex(e => e.Status);
-            entity.HasIndex(e => e.IdempotencyKey).IsUnique();
-            entity.HasIndex(e => e.CreatedAt);
-            entity.HasIndex(e => e.TenantId);
-            entity.Property(e => e.IdempotencyKey).IsRequired().HasMaxLength(100);
-            entity.Property(e => e.Subtotal).HasPrecision(10, 2);
-            entity.Property(e => e.DiscountTotal).HasPrecision(10, 2);
-            entity.Property(e => e.TaxAmount).HasPrecision(10, 2);
-            entity.Property(e => e.Total).HasPrecision(10, 2);
-            entity.Property(e => e.RefundAmount).HasPrecision(10, 2);
-            entity.Property(e => e.Currency).HasMaxLength(3).HasDefaultValue("USD");
-            entity.Property(e => e.PaymentProviderReference).HasMaxLength(200);
-            entity.Property(e => e.PaymentMethod).HasMaxLength(50);
-            entity.Property(e => e.IpAddress).HasMaxLength(45);
-            entity.Property(e => e.UserAgent).HasMaxLength(500);
-            entity.Property(e => e.RefundReason).HasMaxLength(500);
-            entity.Property(e => e.Metadata).HasColumnType("jsonb");
-
-            entity.HasMany(e => e.LineItems)
-                .WithOne(li => li.Order)
-                .HasForeignKey(li => li.OrderId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        modelBuilder.Entity<OrderLineItem>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.HasIndex(e => e.OrderId);
-            entity.HasIndex(e => e.ProductId);
-            entity.Property(e => e.ProductNameSnapshot).IsRequired().HasMaxLength(200);
-            entity.Property(e => e.UnitPriceSnapshot).HasPrecision(10, 2);
-            entity.Property(e => e.BasePriceSnapshot).HasPrecision(10, 2);
-            entity.Property(e => e.SalePriceSnapshot).HasPrecision(10, 2);
-            entity.Property(e => e.DiscountAmount).HasPrecision(10, 2);
-            entity.Property(e => e.LineTotal).HasPrecision(10, 2);
-            entity.Property(e => e.PromoCodesApplied).HasMaxLength(500);
-            entity.Property(e => e.PricingTierNameSnapshot).HasMaxLength(100);
-            entity.Property(e => e.BillingIntervalSnapshot).HasMaxLength(20);
-
-            entity.HasOne(e => e.Product)
-                .WithMany()
-                .HasForeignKey(e => e.ProductId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            entity.HasOne(e => e.UserProduct)
-                .WithMany()
-                .HasForeignKey(e => e.UserProductId)
-                .OnDelete(DeleteBehavior.SetNull);
-        });
+        // Note: Order and OrderLineItem configurations are now in GameGuild.Commerce.Orders module
     }
 }
