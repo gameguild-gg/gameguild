da302b85970e8c72b524ae77eaaf45d6392ed88a
Miguel Moroni <miguelmcontato@gmail.com>
2026-04-19 13:09:30 -0300

    refactor(editor): part 1 - remove files

--- apps/web/src/components/editor/extras/multi-block/constants.ts
+++
@@ -1,7 +0,0 @@
-export const PANEL_SYNC_DELAY = 500 // ms to wait before auto-sync after loading project
-export const DEFAULT_PANEL_SIZE = 100
-export const NEW_PANEL_SIZE = 30
-export const MIN_PANEL_SIZE = 10
-export const COLLAPSED_PANEL_SIZE = 3
-export const PANEL_ID_PREFIX = "panel-"
-export const DRAG_ACTIVATION_DISTANCE = 8
