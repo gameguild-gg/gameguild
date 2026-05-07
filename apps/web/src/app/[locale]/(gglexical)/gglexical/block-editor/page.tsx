a0793dc76129bab70c2e35a7d0a0201583915c4d
Miguel Moroni <miguelmcontato@gmail.com>
2026-05-07 14:35:52 -0300

    feat(block-editor): block-editor page

---
+++ apps/web/src/app/[locale]/(gglexical)/gglexical/block-editor/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { EditorProvider } from "@/components/block-content-editor/engines/editor-provider"
+import { EditorToolbar } from "@/components/block-content-editor/engines/editor-toolbar"
+import { EditorField } from "@/components/block-content-editor/engines/editor-field"
+import { EditorDialogs } from "@/components/block-content-editor/engines/editor-dialogs"
+import { StudioLayout } from "../studio/studio-layout"
+import type { FieldConfig, ToolbarConfig } from "@/components/block-content-editor/engines/editor-config"
+
+const fieldConfig: Partial<FieldConfig> = {
+  engines: ["blocks"],
+  defaultEngine: "blocks",
+  defaultMode: "free-page",
+}
+
+const toolbarConfig: Partial<ToolbarConfig> = {
+}
+
+export default function DocEditorPage() {
+  return (
+    <EditorProvider fieldConfig={fieldConfig} toolbarConfig={toolbarConfig}>
+      <StudioLayout>
+        <EditorToolbar />
+        <EditorField />
+      </StudioLayout>
+      <EditorDialogs />
+    </EditorProvider>
+  )
+}
