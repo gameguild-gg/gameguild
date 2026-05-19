ea37666a7fd56ab33f951d493635d31c48eac819
Miguel Moroni <miguelmcontato@gmail.com>
2026-05-19 01:23:08 -0300

    refactor(block-content-editor): audio node without lexical

--- apps/web/src/components/block-content-editor/nodes/audio-node.tsx
+++ apps/web/src/components/block-content-editor/nodes/audio-node.tsx
@@ -1,8 +1,4 @@
-"use client"
-
-import { type SerializedLexicalNode } from "lexical"
-import { MediaNodeBase, type BaseMediaData, type SerializedMediaNode } from "./base/media-node-base"
-import { MediaComponent } from "./media-component"
+import type { SerializedBlockNode } from "./base/serialized-block-node"
 
 export interface AudioData {
   src: string
@@ -14,69 +10,4 @@ export interface AudioData {
   artist?: string
 }
 
-export interface SerializedAudioNode extends SerializedLexicalNode {
-  type: "audio"
-  data: AudioData
-  version: 1
-}
-
-export class AudioNode extends MediaNodeBase {
-  getType(): string {
-    return "audio"
-  }
-
-  static getType(): string {
-    return "audio"
-  }
-
-  clone(node: AudioNode): AudioNode {
-    return new AudioNode(node.__data as BaseMediaData, node.__key)
-  }
-
-  static clone(node: AudioNode): AudioNode {
-    return new AudioNode(node.__data as BaseMediaData, node.__key)
-  }
-
-  constructor(data: AudioData | BaseMediaData, key?: string) {
-    const baseData: BaseMediaData = {
-      type: "audio",
-      src: data.src,
-      caption: data.caption,
-      size: data.size ?? 100,
-      isNew: (data as AudioData).isNew || (data as BaseMediaData).isNew,
-      audioType: (data as AudioData).type || (data as BaseMediaData).audioType,
-      embedAudioType: (data as BaseMediaData).embedAudioType || "direct",
-    }
-    super(baseData, key)
-  }
-
-  createDOM(): HTMLElement {
-    const div = document.createElement("div")
-    div.style.display = "contents"
-    return div
-  }
-
-  updateDOM(): false {
-    return false
-  }
-
-  exportJSON(): SerializedMediaNode {
-    return {
-      type: "audio",
-      data: this.__data,
-      version: 1,
-    }
-  }
-
-  static importJSON(serializedNode: SerializedAudioNode | SerializedMediaNode): AudioNode {
-    return new AudioNode(serializedNode.data)
-  }
-
-  decorate(): React.JSX.Element {
-    return <MediaComponent data={this.__data} nodeKey={this.__key} NodeClass={AudioNode} />
-  }
-}
-
-export function $createAudioNode(data: AudioData | BaseMediaData): AudioNode {
-  return new AudioNode(data)
-}
+export type SerializedAudioNode = SerializedBlockNode<"audio", AudioData>
